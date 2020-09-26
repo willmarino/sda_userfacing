efd8e2ce95d4abd8e52a959351cf878e68cab9df
Will <w.marino997@gmail.com>
2020-09-26 15:43:22 -0400

    predictions fetch with filters functional and styled

--- frontend/src/components/buttons/execute_fetch_button/execute_fetch_button.js
+++ frontend/src/components/buttons/execute_fetch_button/execute_fetch_button.js
@@ -4,16 +4,29 @@ import React from 'react';
 class ExecuteFetchButton extends React.Component{
   constructor(props){
     super(props);
+    this.state = {
+      buttonId: null
+    }
     this.handleClick = this.handleClick.bind(this);
+    this.switchButtonColor = this.switchButtonColor.bind(this);
   }
   handleClick(){
     const { fetchPredictions, fetchConfig } = this.props;
     fetchPredictions(fetchConfig);
+    this.switchButtonColor();
+  }
+  switchButtonColor(){
+    this.setState({ buttonId: 'exec-fetch-button-active' }, () => {
+      setTimeout(() => {
+        this.setState({ buttonId: null });
+      }, 1000)
+    })
   }
   render(){
+    let { buttonId } = this.state;
     return(
-      <div className="exec-fetch-button" onClick={this.handleClick}>
-        <p>search!</p>
+      <div id={buttonId} className="exec-fetch-button" onClick={this.handleClick}>
+        <p>Go</p>
       </div>
     )
   }
